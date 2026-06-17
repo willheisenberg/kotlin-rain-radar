79bd43e15aeb9eea481c47f8b1af901a2ac6fd5c
danzicchi@gmail.com <danzicchi@gmail.com>
2026-06-17 12:10:22 +0200

    chore: bump version code to 2 and version name to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.willheisenberg.openrain"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.2.0"
+        versionCode = 2
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
