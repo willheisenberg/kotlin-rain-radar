2538ee1029479ac6cf1a0a7827e69425c2566260
danzicchi@gmail.com <danzicchi@gmail.com>
2026-06-21 12:26:47 +0200

    chore: bump version code to 4 and version name to 0.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.willheisenberg.openrain"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.2"
+        versionCode = 4
+        versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
